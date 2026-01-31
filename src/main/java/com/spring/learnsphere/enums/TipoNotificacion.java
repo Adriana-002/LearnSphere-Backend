e79355da09ee587c89c90480e77a023ae579202b
Adriana <agomezs20@educantabria.es>
2026-01-31 23:08:10 +0100

    Creacion de enum para el arreglo de la entidad Notificacion :ambulance:

---
+++ src/main/java/com/spring/learnsphere/enums/TipoNotificacion.java
@@ -0,0 +1,10 @@
+package com.spring.learnsphere.enums;
+
+public enum TipoNotificacion {
+    nueva_nota,
+    nueva_falta,
+    nueva_observacion,
+    nuevo_mensaje,
+    nuevo_aviso,
+    recordatorio_material
+}
