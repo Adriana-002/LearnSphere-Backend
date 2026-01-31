79a5f1417cad0a21d937d67164f3693fa8f6c295
Adriana <agomezs20@educantabria.es>
2026-01-31 23:31:57 +0100

    Añadidos los controladores correspondientes a los servicios :sparkles:

---
+++ src/main/java/com/spring/learnsphere/controller/AlumnoController.java
@@ -0,0 +1,13 @@
+package com.spring.learnsphere.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/alumnos")
+public class AlumnoController {
+
+
+}
---
+++ src/main/java/com/spring/learnsphere/controller/CursoController.java
@@ -0,0 +1,12 @@
+package com.spring.learnsphere.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cursos")
+public class CursoController {
+
+}
