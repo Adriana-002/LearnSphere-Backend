8ef0eeaa4b78a43c54e4bce514a68d18e84e7637
Adriana <agomezs20@educantabria.es>
2026-02-24 01:28:16 +0100

    Prueba de conexion back con front :poop:

--- src/main/java/com/spring/learnsphere/controller/AlumnoController.java
+++ src/main/java/com/spring/learnsphere/controller/AlumnoController.java
@@ -1,13 +1,24 @@
 package com.spring.learnsphere.controller;
 
+import com.spring.learnsphere.model.Alumno;
+import com.spring.learnsphere.service.AlumnoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/alumnos")
 public class AlumnoController {
+    @Autowired
+    private final AlumnoService alumnoService;
 
-
+    @GetMapping("/listar")
+    public List<Alumno> listarAlumnos() {
+        return alumnoService.getAllAlumnos();
+    }
 }
--- src/main/java/com/spring/learnsphere/service/AlumnoService.java
+++ src/main/java/com/spring/learnsphere/service/AlumnoService.java
@@ -1,10 +1,20 @@
 package com.spring.learnsphere.service;
 
+import com.spring.learnsphere.model.Alumno;
+import com.spring.learnsphere.repository.AlumnoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AlumnoService {
+    @Autowired
+    private final AlumnoRepository alumnoRepository;
 
+    public List<Alumno> getAllAlumnos() {
+        return alumnoRepository.findAll();
+    }
 }
